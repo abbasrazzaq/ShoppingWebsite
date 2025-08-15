4545cda68a4351c5255521e2acf63f544524082e
abbas <abbas@ABBAS-THINKPAD>
2025-08-15 18:50:11 +0100

    Adding ability to "add to cart"

--- shoppingwebsite.client/src/pages/Shop.jsx
+++ shoppingwebsite.client/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 function Shop() {
     const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [cartItems, setCartItems] = useState([]);
 
     useEffect(() => {
         async function populateShopItems() {
@@ -28,13 +29,21 @@ function Shop() {
 
     if (loading) return <div>Loading items...</div>;
 
+    function addItemToCart(id) {
+        setCartItems((prev) => ({
+            ...prev,
+            [id]: (prev[id] || 0) + 1,
+        }));
+    };
+
     return (
         <div>
             <h2>Shop Items</h2>
             <ul>
                 {items.map((item) => (
                     <li key={item.id}>
-                        {item.name} - ${item.price.toFixed(2)}
+                        {item.name} - ${item.price.toFixed(2)}. {item.stock - (cartItems[item.id] || 0)} Left.
+                        <button onClick={() => addItemToCart(item.id)}>Add to cart</button>
                     </li>
                 ))}
             </ul>
